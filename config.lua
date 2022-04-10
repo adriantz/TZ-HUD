17fe0d28f7f5ebfadba60d472e5dbe1f01bada80
ADRIANTZ <103338093+adriantz@users.noreply.github.com>
2022-04-10 10:32:44 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,17 @@
+TZ = {}
+TZ.HideOnHidedRadar = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- TZ-Community
---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TZ Community | ADRIANTZ'
+description 'A simple HUD'
+version '1.0'
+
+client_scripts {
+    'client.lua',
+    'config.lua'
+}
+
+ui_page 'html/ui.html'
+files {
+	'html/ui.html',
+	'html/css/style.css',
+	'html/css/grid.css',
+	'html/js/main.js',
+	'html/img/*',
+	'html/fonts/*'
+}
